4631d1132b7e25b5a77bab959a4c0e0dff97a631
nabiikaz <nabiikaz2017@gmail.com>
2019-06-26 21:58:56 +0200

    update wp-config.php
    - changing the database credentials -

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'bitnami_wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'bn_wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '5aa88bbda2');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
